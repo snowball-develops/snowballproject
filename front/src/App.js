c6184f2ae85e1a0dd54affd9459a5cd67434abcc
최정훈/Hamoonrang <hamoonrang@gmail.com>
2022-02-10 17:34:11 +0900

    콜록콜록

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
+/* Design Components */
+import Top from './components/design/Top'
 
 function App() {
   return (
+    <>
+    <Top/>
     <div className="App">
+      HI
     </div>
+    </>
   );
 }
 
---
+++ front/src/components/design/Top.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Top() {
+  return (
+    <nav className='TopBar'>
+       <span>1</span>
+       <span>2</span>
+       <span>3</span>
+       <span>4</span>
+    </nav>
+  )
+}
+
+export default Top
