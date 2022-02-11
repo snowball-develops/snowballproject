6b148cb2f94ffb771462dd853aa59622b690002f
최정훈/Hamoonrang <hamoonrang@gmail.com>
2022-02-11 15:53:00 +0900

    집에서하자

--- front/src/components/design/Top.jsx
+++ front/src/components/design/Top.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Top() {
   return (
     <nav className='TopBar'>
+      <header><h1></h1></header>
        <span>1</span>
        <span>2</span>
        <span>3</span>
