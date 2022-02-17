7588629ab80674070d36f7b69810e827a7e7bd63
최정훈/Hamoonrang <hamoonrang@gmail.com>
2022-02-17 16:58:23 +0900

    몰루

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,22 @@
 import { useState } from 'react';
 import './App.css';
-import {FiMenu} from 'react-icons/fi'
+import { FiMenu } from 'react-icons/fi'
 /* Design Components */
 import Top from './components/design/Top'
 import Home from './components/routes/Home';
 
 function App() {
   const [onTopOffTop, setonTopOffTop] = useState(false);
-  const onOff = ()=>{
+  const onOff = () => {
     setonTopOffTop(!onTopOffTop)
   }
   return (
     <>
-    <span className='callMenu' onClick={onOff}><FiMenu/></span>
-    {onTopOffTop ? <Top classed={"TopBar"}/> : <Top classed={"TopBar2"}/>}
-    <div className="App">
-      <Home/>
-    </div>
+      <span className='callMenu' onClick={onOff}><FiMenu /></span>
+      {onTopOffTop ? <Top classed={"TopBar"} /> : <Top classed={"TopBar2"} />}
+      <div className="App">
+        <Home />
+      </div>
     </>
   );
 }
--- front/src/components/routes/Home.jsx
+++ front/src/components/routes/Home.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import Fade from 'react-reveal/Fade';
 
 function Home() {
+  const sections = useRef();
+  const onHomeClick = () => {
+    sections.current.scrollIntoView({ behavior: 'smooth' });
+      };
   return (
     <div className='Home'>
+      <div ref={sections}></div>
       <h2>Titles</h2>
-      <Fade right>
         <div className='sections'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, eligendi assumenda? Dolorum placeat quisquam adipisci distinctio alias minus illo. Autem dolores consequuntur recusandae rem, quibusdam error tenetur incidunt dolor assumenda cumque doloremque eum saepe sit vitae ipsa? Delectus quam, asperiores inventore et odit, esse error labore sed voluptas dolor reprehenderit, soluta quo temporibus dolorem velit saepe accusantium animi. Unde eligendi fugiat debitis! Perferendis, obcaecati possimus doloremque recusandae tempore quod similique placeat, dolor voluptates asperiores tenetur cupiditate? Reprehenderit nesciunt ea voluptatum neque libero repellendus, corrupti cupiditate magnam quo a! Molestiae quam, natus tempora minus deserunt quibusdam minima ut aliquid corporis qui.</div>
-      </Fade>
-      <Fade left>
       <div className='sections'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, eligendi assumenda? Dolorum placeat quisquam adipisci distinctio alias minus illo. Autem dolores consequuntur recusandae rem, quibusdam error tenetur incidunt dolor assumenda cumque doloremque eum saepe sit vitae ipsa? Delectus quam, asperiores inventore et odit, esse error labore sed voluptas dolor reprehenderit, soluta quo temporibus dolorem velit saepe accusantium animi. Unde eligendi fugiat debitis! Perferendis, obcaecati possimus doloremque recusandae tempore quod similique placeat, dolor voluptates asperiores tenetur cupiditate? Reprehenderit nesciunt ea voluptatum neque libero repellendus, corrupti cupiditate magnam quo a! Molestiae quam, natus tempora minus deserunt quibusdam minima ut aliquid corporis qui.</div>
-      </Fade>
-      <Fade right>
       <div className='sections'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, eligendi assumenda? Dolorum placeat quisquam adipisci distinctio alias minus illo. Autem dolores consequuntur recusandae rem, quibusdam error tenetur incidunt dolor assumenda cumque doloremque eum saepe sit vitae ipsa? Delectus quam, asperiores inventore et odit, esse error labore sed voluptas dolor reprehenderit, soluta quo temporibus dolorem velit saepe accusantium animi. Unde eligendi fugiat debitis! Perferendis, obcaecati possimus doloremque recusandae tempore quod similique placeat, dolor voluptates asperiores tenetur cupiditate? Reprehenderit nesciunt ea voluptatum neque libero repellendus, corrupti cupiditate magnam quo a! Molestiae quam, natus tempora minus deserunt quibusdam minima ut aliquid corporis qui.</div>
-      </Fade>
+      <button onClick={onHomeClick}>dasdasd</button>
+
     </div>
   )
 }
