d1579d23fdc6548bc1b32c9a75be00a6d2db169b
최정훈/Hamoonrang <hamoonrang@gmail.com>
2022-02-17 14:23:57 +0900

    asf

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import {FiMenu} from 'react-icons/fi'
 /* Design Components */
 import Top from './components/design/Top'
 import Home from './components/routes/Home';
@@ -11,7 +12,7 @@ function App() {
   }
   return (
     <>
-    <span className='callMenu' onClick={onOff}>Click</span>
+    <span className='callMenu' onClick={onOff}><FiMenu/></span>
     {onTopOffTop ? <Top classed={"TopBar"}/> : <Top classed={"TopBar2"}/>}
     <div className="App">
       <Home/>
