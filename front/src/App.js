3ab431352a462288da508c81ad6ca1c978e50c0d
최정훈/Hamoonrang <hamoonrang@gmail.com>
2022-02-18 11:17:05 +0900

    변경변경

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 import './App.css';
 import { FiMenu } from 'react-icons/fi'
 /* Design Components */
@@ -10,12 +10,19 @@ function App() {
   const onOff = () => {
     setonTopOffTop(!onTopOffTop)
   }
+
+  const sections = useRef();
+  const onHomeClick = () => {
+    sections.current.scrollIntoView({ behavior: 'smooth' });
+  };
   return (
     <>
       <span className='callMenu' onClick={onOff}><FiMenu /></span>
+      <div ref={sections}></div>
       {onTopOffTop ? <Top classed={"TopBar"} /> : <Top classed={"TopBar2"} />}
       <div className="App">
         <Home />
+      <button onClick={onHomeClick}>dasdasd</button>
       </div>
     </>
   );
--- front/src/components/routes/Home.jsx
+++ front/src/components/routes/Home.jsx
@@ -1,19 +1,31 @@
-import React,{useRef} from 'react'
+import React, { useRef } from 'react'
 import Fade from 'react-reveal/Fade';
 
 function Home() {
-  const sections = useRef();
-  const onHomeClick = () => {
-    sections.current.scrollIntoView({ behavior: 'smooth' });
-      };
+  const sections2 = useRef();
+  const sections3 = useRef();
+
+  const onsections2 = () => {
+    sections2.current.scrollIntoView({ behavior: 'smooth' });
+  };
+  const onsections3 = () => {
+    sections3.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className='Home'>
-      <div ref={sections}></div>
       <h2>Titles</h2>
-        <div className='sections'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, eligendi assumenda? Dolorum placeat quisquam adipisci distinctio alias minus illo. Autem dolores consequuntur recusandae rem, quibusdam error tenetur incidunt dolor assumenda cumque doloremque eum saepe sit vitae ipsa? Delectus quam, asperiores inventore et odit, esse error labore sed voluptas dolor reprehenderit, soluta quo temporibus dolorem velit saepe accusantium animi. Unde eligendi fugiat debitis! Perferendis, obcaecati possimus doloremque recusandae tempore quod similique placeat, dolor voluptates asperiores tenetur cupiditate? Reprehenderit nesciunt ea voluptatum neque libero repellendus, corrupti cupiditate magnam quo a! Molestiae quam, natus tempora minus deserunt quibusdam minima ut aliquid corporis qui.</div>
-      <div className='sections'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, eligendi assumenda? Dolorum placeat quisquam adipisci distinctio alias minus illo. Autem dolores consequuntur recusandae rem, quibusdam error tenetur incidunt dolor assumenda cumque doloremque eum saepe sit vitae ipsa? Delectus quam, asperiores inventore et odit, esse error labore sed voluptas dolor reprehenderit, soluta quo temporibus dolorem velit saepe accusantium animi. Unde eligendi fugiat debitis! Perferendis, obcaecati possimus doloremque recusandae tempore quod similique placeat, dolor voluptates asperiores tenetur cupiditate? Reprehenderit nesciunt ea voluptatum neque libero repellendus, corrupti cupiditate magnam quo a! Molestiae quam, natus tempora minus deserunt quibusdam minima ut aliquid corporis qui.</div>
-      <div className='sections'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, eligendi assumenda? Dolorum placeat quisquam adipisci distinctio alias minus illo. Autem dolores consequuntur recusandae rem, quibusdam error tenetur incidunt dolor assumenda cumque doloremque eum saepe sit vitae ipsa? Delectus quam, asperiores inventore et odit, esse error labore sed voluptas dolor reprehenderit, soluta quo temporibus dolorem velit saepe accusantium animi. Unde eligendi fugiat debitis! Perferendis, obcaecati possimus doloremque recusandae tempore quod similique placeat, dolor voluptates asperiores tenetur cupiditate? Reprehenderit nesciunt ea voluptatum neque libero repellendus, corrupti cupiditate magnam quo a! Molestiae quam, natus tempora minus deserunt quibusdam minima ut aliquid corporis qui.</div>
-      <button onClick={onHomeClick}>dasdasd</button>
+      <div className='sections'>
+        <Fade left>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, eligendi assumenda? Dolorum placeat quisquam adipisci distinctio alias minus illo. Autem dolores consequuntur recusandae rem, quibusdam error tenetur incidunt dolor assumenda cumque doloremque eum saepe sit vitae ipsa? Delectus quam, asperiores inventore et odit, esse error labore sed voluptas dolor reprehenderit, soluta quo temporibus dolorem velit saepe accusantium animi. Unde eligendi fugiat debitis! Perferendis, obcaecati possimus doloremque recusandae tempore quod similique placeat, dolor voluptates asperiores tenetur cupiditate? Reprehenderit nesciunt ea voluptatum neque libero repellendus, corrupti cupiditate magnam quo a! Molestiae quam, natus tempora minus deserunt quibusdam minima ut aliquid corporis qui. </Fade>
+      <button onClick={onsections2}>dasdasd</button>
+      </div>
+      <div ref={sections2} className='sections'>
+        <Fade right>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, eligendi assumenda? Dolorum placeat quisquam adipisci distinctio alias minus illo. Autem dolores consequuntur recusandae rem, quibusdam error tenetur incidunt dolor assumenda cumque doloremque eum saepe sit vitae ipsa? Delectus quam, asperiores inventore et odit, esse error labore sed voluptas dolor reprehenderit, soluta quo temporibus dolorem velit saepe accusantium animi. Unde eligendi fugiat debitis! Perferendis, obcaecati possimus doloremque recusandae tempore quod similique placeat, dolor voluptates asperiores tenetur cupiditate? Reprehenderit nesciunt ea voluptatum neque libero repellendus, corrupti cupiditate magnam quo a! Molestiae quam, natus tempora minus deserunt quibusdam minima ut aliquid corporis qui.</Fade>
+      <button onClick={onsections3}>dasdasd</button>
+      </div>
+      <div ref={sections3} className='sections'>
+        <Fade left>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, eligendi assumenda? Dolorum placeat quisquam adipisci distinctio alias minus illo. Autem dolores consequuntur recusandae rem, quibusdam error tenetur incidunt dolor assumenda cumque doloremque eum saepe sit vitae ipsa? Delectus quam, asperiores inventore et odit, esse error labore sed voluptas dolor reprehenderit, soluta quo temporibus dolorem velit saepe accusantium animi. Unde eligendi fugiat debitis! Perferendis, obcaecati possimus doloremque recusandae tempore quod similique placeat, dolor voluptates asperiores tenetur cupiditate? Reprehenderit nesciunt ea voluptatum neque libero repellendus, corrupti cupiditate magnam quo a! Molestiae quam, natus tempora minus deserunt quibusdam minima ut aliquid corporis qui. </Fade>
+      </div>
 
     </div>
   )
