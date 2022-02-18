eeee7aec5c793ab22f202a673e88c16324d7359e
최정훈/Hamoonrang <hamoonrang@gmail.com>
2022-02-18 13:24:04 +0900

    업데이트

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,13 @@
-import { useState,useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './App.css';
 import { FiMenu } from 'react-icons/fi'
+import {FaArrowUp} from 'react-icons/fa'
 /* Design Components */
 import Top from './components/design/Top'
 import Home from './components/routes/Home';
 
 function App() {
+  const [scrollPosition, setScrollPosition] = useState(0);
   const [onTopOffTop, setonTopOffTop] = useState(false);
   const onOff = () => {
     setonTopOffTop(!onTopOffTop)
@@ -15,6 +17,14 @@ function App() {
   const onHomeClick = () => {
     sections.current.scrollIntoView({ behavior: 'smooth' });
   };
+
+  const updateScroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', updateScroll);
+  });
+
   return (
     <>
       <span className='callMenu' onClick={onOff}><FiMenu /></span>
@@ -22,7 +32,7 @@ function App() {
       {onTopOffTop ? <Top classed={"TopBar"} /> : <Top classed={"TopBar2"} />}
       <div className="App">
         <Home />
-      <button onClick={onHomeClick}>dasdasd</button>
+        <span className={scrollPosition < 550 ? "toTop" : "toTop2"} onClick={onHomeClick}><FaArrowUp/></span>
       </div>
     </>
   );
